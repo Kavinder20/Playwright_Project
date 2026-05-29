767997d80ea0ef4976fd637375d1b37dfea93720
Kavinder <Kavindersingh588@gmail.com>
2026-05-29 19:59:32 +0200

    Practiced Playwright special locators and dropdown handling

--- e2e/CompleteDemo.spec.js
+++ e2e/CompleteDemo.spec.js
@@ -10,7 +10,7 @@ test.only('Browser Context Playwright test', async ({ page }) => {
     await page.locator("#userPassword").fill("Letsmakeitbig@play1");
     await page.locator("[value='Login']").click();
 
-     await page.waitForLoadState('networkidle');
+     await page.waitForLoadState('networkidle'); // waits for all network requests
 
       await page.locator(".card-body b").first().waitFor();
 
@@ -38,7 +38,33 @@ test.only('Browser Context Playwright test', async ({ page }) => {
   
   await page.locator("text=Checkout").click();
 
-  await page.pause();
+await page.locator("input[placeholder*='Select Country']").pressSequentially("ger", {delay:100}) ;
+const  dropDown = await page.locator(".ta-results");
+await dropDown.waitFor();
+
+const optionsCount = await dropDown.locator("button").count();
+for(let i = 0; i<optionsCount ; i++)
+{
+
+  const text = await dropDown.locator("button").nth(i).textContent();
+  
+  if(text.trim() === 'Germany')
+  {
+    await dropDown.locator("button").nth(i).click();
+     break ;
+  }
+   
+<span _ngcontent-xjv-c37="" class="ng-star-inserted" xpath="1"><i _ngcontent-xjv-c37="" class="fa fa-search"></i> Germany</span>
+}
+//expect(page.locator(".user__name [type*='text']").first()).toHaveText(email);
+
+//await page.locator(".action__submit").click();
+//await page.locator(".hero-primary").toHaveText("Thankyou for the order");
+
+    expect(page.locator(".user__name  [type='text']").first()).toHaveText(email);
+    await page.locator(".action__submit").click();
+    await expect(page.locator(".hero-primary")).toHaveText("  Thankyou for the order. ");
+
 
     
   
---
+++ e2e/practice.spec.js
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+ 
+test('Playwright Special locators', async ({ page }) => {
+  
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+  // await page.getbyLabel("Check me out if you Love IceCreams!").click();
+   await page.getByLabel("Check me out if you Love IceCreams!").click();
+   await page.getByLabel("Gender").selectOption("Female");
+   await page.getByLabel("Student").click();
+const visible = await page
+  .getByText("The Form has been submitted successfully!.")
+  .isVisible();
+
+console.log("Visible:", visible);
+   //await page.getByText(" The Form has been submitted successfully!.").isVisible();
+
+ 
+});
